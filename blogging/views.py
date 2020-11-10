7a363e4d9a942ac044602c595ad5ce28b612236e
Ian Canavan <icanavan.327@gmail.com>
2020-11-09 21:35:21 -0800

    end of activity

--- blogging/views.py
+++ blogging/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.template import loader
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from blogging.models import Post
 
 def list_view(request):
-    context = {'posts': Post.objects.all()}
-    return render(request, 'blogging/list.html', context)
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
 
 def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
     try:
-        post = Post.objects.get(pk=post_id)
+        post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
-
-
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
