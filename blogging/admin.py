ae0c44d0ef7316eaa31d6bfa8b3381720282e349
Ian Canavan <icanavan.327@gmail.com>
2020-11-09 22:20:03 -0800

    not quite complete with this, I feel like I'm close but can't get the page to load to check. This is probably due to some errors in the classes

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+    exclude = ('posts')
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    class Meta:
+        model = Post
+    # fieldsets = (None, {
+    #         'fields': ('url', 'title', 'content', 'categories')
+    #     })
+    pass
+
 admin.site.register(Post)
+admin.site.register(PostAdmin)
 admin.site.register(Category)
+admin.site.register(CategoryAdmin)
